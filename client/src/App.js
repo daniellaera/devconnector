cc84536c3e0b3d04cf0f2ffc36d4367ea02f450e
Daniel Laera <laera.daniel@hotmail.com>
2018-08-15 19:10:59 +0200

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,10 @@ import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from './components/dashboard/Dashboard';
 
 import './App.css';
+import { clearCurrentProfile } from './actions/profileActions';
 
 // check for token
 if (localStorage.jwtToken) {
@@ -29,7 +31,8 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     // Logout the user
     store.dispatch(logoutUser());
-    // TODO: clear current profile
+    // Clear current profile
+    store.dispatch(clearCurrentProfile());
     // redirect to login
     window.location.href = '/login';
   }
@@ -47,6 +50,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component= { Register } />
               <Route exact path="/login" component= { Login } />
+              <Route exact path="/dashboard" component= { Dashboard } />
             </div>
             <Footer />
           </div>
