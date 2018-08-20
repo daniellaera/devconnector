4182014a0e179dafdde07de851ba5ac5a2c67a66
Daniel Laera <laera.daniel@hotmail.com>
2018-08-20 17:09:28 +0200

    spinner and dashboard component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from 'react-redux';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
 import store from './store';
+
+import PrivateRoute from './components/common/PrivateRoute'
 
 
 import Navbar from "./components/layout/Navbar";
@@ -50,7 +52,9 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component= { Register } />
               <Route exact path="/login" component= { Login } />
-              <Route exact path="/dashboard" component= { Dashboard } />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component= { Dashboard } />
+              </Switch>
             </div>
             <Footer />
           </div>
