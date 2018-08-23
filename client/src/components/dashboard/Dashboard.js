060b15f797ceeea256c6d45fb3e5baf9cbbe0f71
Daniel Laera <laera.daniel@hotmail.com>
2018-08-23 10:34:49 +0200

    createProfile component added

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -38,7 +38,7 @@ class Dashboard extends Component {
     return (
       <div className="dashboard">
         <div className="container">
-          <div className="raw">
+          <div className="row">
             <div className="col-md-12">
               <h1 className="display-4">Dashboard</h1>
               {dashboardContent}
