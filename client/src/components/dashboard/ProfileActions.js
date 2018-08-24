7117be12d826a69dd68ae832a48b6c3b9856afd4
Daniel Laera <laera.daniel@hotmail.com>
2018-08-24 11:03:37 +0200

    deleteAccount danger button

--- client/src/components/dashboard/ProfileActions.js
+++ client/src/components/dashboard/ProfileActions.js
@@ -9,7 +9,7 @@ const ProfileActions = () => {
         <i className="fas fa-user-circle text-info mr-1" />Edit Profile
       </Link>
       <Link to="/add-experience" className="btn btn-light">
-        <i className="fas fa-black-tie text-info mr-1" />Add Experience
+        <i className="fas fa-user-tie text-info mr-1" />Add Experience
       </Link>
       <Link to="/add-education" className="btn btn-light">
         <i className="fas fa-graduation-cap text-info mr-1" />Add Education
