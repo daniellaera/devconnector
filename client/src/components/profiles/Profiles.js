ecd971c7df662655d9c1b6972ab1e3620ffbd6fa
Daniel Laera <laera.daniel@hotmail.com>
2018-08-31 13:48:12 +0200

    reducerPost

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -32,7 +32,7 @@ class Profiles extends Component {
             <div className="row">
                 <div className="col-md-12">
                     <h1 className="display-4 text-center">Developer Profiles</h1>
-                    <p className="lead text-center">Browse and Conncet with developers</p>
+                    <p className="lead text-center">Browse and Connect with Developers</p>
                     { profileItems }
                 </div>
             </div>
