48ba0300730e8c4a1b79cd73b29907f1199e8e6b
Daniel Laera <laera.daniel@hotmail.com>
2018-12-29 13:10:56 +0100

    some changes

--- client/src/store.js
+++ client/src/store.js
@@ -6,28 +6,28 @@ const initialState = {};
 
 const middleware = [thunk];
 
-/* const store = createStore(
+const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
-); */
+);
 
-const whichCompose = () => {
+/* const whichCompose = () => {
   if (window.navigator.userAgent.includes("Chrome")) {
     compose(
       applyMiddleware(...middleware),
       // Implements the Chrome redux tools extension
       window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__()
+        window.__REDUX_DEVTOOLS_EXTENSION__()
     );
   } else {
     compose(applyMiddleware(...middleware));
   }
 };
 
-const store = createStore(rootReducer, initialState, whichCompose());
+const store = createStore(rootReducer, {}, whichCompose()); */
 
 export default store;
