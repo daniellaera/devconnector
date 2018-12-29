039b765de93d9126b721b2129373001738e0a776
Daniel Laera <laera.daniel@hotmail.com>
2018-12-29 16:30:04 +0100

    composeEnhancer

--- client/src/store.js
+++ client/src/store.js
@@ -2,18 +2,27 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import reducers from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 
 const initialState = {};
 
-// const middleware = [thunk];
+const middleware = [thunk];
 
-const store = createStore(
+const composeEnhancers = composeWithDevTools({})
+
+const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+  applyMiddleware(...middleware),
+  // other store enhancers if any
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+/* const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(thunk),
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
-);
+); */
 
 export default store;
